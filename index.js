c6bd93c58406d37521cfdda4fa7c20ff0ff99656
Atanas Chompolov <a.chompolov@gmail.com>
2017-01-15 22:43:01 -0800

    Last commit for today
    I have no idea what I’m doing

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+/* Crypto text */
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = 'd6F3Efeq';
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+
 /* Take IPv4 of the machine */
 var os = require('os');
 var interfaces = os.networkInterfaces();
@@ -22,20 +41,38 @@ http.listen(port, ip, function() {
   console.log(`Server running at http://${ip}:${port}`);
 });
 
-socketIO.on('connection', function(clientSocket){
+socketIO.on('connection', function(socket){
   var clientIp = socket.request.connection.remoteAddress;
 
   console.log(`Client with IP:${clientIp} has Connected.`);
 
-  clientSocket.on('disconnect', function() {
+  socket.on('disconnect', function() {
     console.log(`Client with IP:${clientIp} has Disconnected`);
   });
 
-  clientSocket.on('signUpClicked', function(signUpData) {
-    console.log(signUpData);
+  socket.on('signUpClicked', function(signUpData) {
+    var signUpDataArray = signUpData.split(' | ');
+
+    var fullName = signUpDataArray[0].split(' ');//Separatin first and last name
+    var firstName = fullName[0];
+    var lastName = fullName[1];
+
+    var email = signUpDataArray[1];
+    var username = signUpDataArray[2];
+    var password = encrypt(signUpDataArray[3]);
+
+    var birthDate = signUpDataArray[4];
+
+    var gender = signUpDataArray[5];
+
+    console.log(`firstName: ${firstName}\nlastName: ${lastName}\nemail: ${email}\nusername${username}\npassword: ${password}\nbirthDate: ${birthDate}\ngender: ${gender}\n`);
   });
 
-  clientSocket.on('logInClicked', function(logInData) {
-    console.log(logInData);
+  socket.on('logInClicked', function(logInData) {
+    var logInDataArray = logInData.split(' | ');
+    var username = logInDataArray[0];
+    var password = encrypt(logInDataArray[1]);
+
+    console.log(`username: ${username}\npassword: ${password}\n`);
   });
 });
